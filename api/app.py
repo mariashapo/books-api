6c636f73ea3b8e93f35a6d04e110db7d136e2065
mariashapo <mart99maria@gmail.com>
2024-01-29 18:26:26 +0000

    Adding parameter filtering to the API

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -43,7 +43,26 @@ books = [
 
 @app.route('/books', methods=['GET'])
 def get_books():
-    return jsonify(books)
+    genre = request.args.get('genre')
+    author = request.args.get('author')
+    title = request.args.get('title')
+
+    filtered_books = []
+    
+    if genre or author or title:
+        for book in books:
+            genre_match = str.lower(genre) in str.lower(book['genre']) if genre else True
+            author_match = str.lower(author) in str.lower(book['author']) if author else True
+            title_match = str.lower(title) in str.lower(book['title']) if title else True
+
+            # Add the book if it matches both genre and author criteria
+            if genre_match and author_match and title_match:
+                filtered_books.append(book)
+    else:
+        # If no filters are provided, use all books
+        filtered_books = books
+
+    return jsonify(filtered_books)
 
 if __name__ == '__main__':
     app.run(debug=True)
